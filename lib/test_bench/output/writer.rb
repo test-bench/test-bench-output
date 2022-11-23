de701f8a3f40b5aa27de5a8e4bb708e4bae1a700
Nathan Ladd <nathanladd@gmail.com>
2022-11-23 14:31:51 -0600

    Writer: Flush

--- lib/test_bench/output/writer.rb
+++ lib/test_bench/output/writer.rb
@@ -6,8 +6,22 @@ module TestBench
       end
       attr_writer :device
 
+      def alternate_device
+        @alternate_device ||= Device::Substitute.build
+      end
+      attr_writer :alternate_device
+
+      def buffer
+        @buffer ||= Buffer.new
+      end
+      attr_writer :buffer
+
       def tty?
         device.tty?
+      end
+
+      def flush
+        buffer.flush(device, alternate_device)
       end
     end
   end
