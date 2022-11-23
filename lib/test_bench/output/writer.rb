1eeface25d59bf133e8fc40511d2db6e64e546ec
Nathan Ladd <nathanladd@gmail.com>
2022-11-23 16:30:38 -0600

    Writer: Write

--- lib/test_bench/output/writer.rb
+++ lib/test_bench/output/writer.rb
@@ -11,6 +11,16 @@ module TestBench
       end
       attr_writer :alternate_device
 
+      def digest
+        @digest ||= Digest.new
+      end
+      attr_writer :digest
+
+      def sequence
+        @sequence ||= 0
+      end
+      attr_writer :sequence
+
       def buffer
         @buffer ||= Buffer.new
       end
@@ -18,6 +28,24 @@ module TestBench
 
       def sync
         @sync.nil? ? @sync = true : @sync
+      end
+
+      def write(data)
+        if sync
+          device.write(data)
+          alternate_device.write(data)
+
+          bytes_written = data.bytesize
+        else
+          bytes_written = buffer.receive(data)
+        end
+
+        self.sequence += bytes_written
+
+        data = data[0...bytes_written]
+        digest.update(data)
+
+        bytes_written
       end
 
       def tty?
