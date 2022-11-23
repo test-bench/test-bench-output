1dc2bfd2a415a17f0a8189fb8ce65fbb210b4986
Nathan Ladd <nathanladd@gmail.com>
2022-11-23 16:19:49 -0600

    Writer: Sync

--- lib/test_bench/output/writer.rb
+++ lib/test_bench/output/writer.rb
@@ -16,12 +16,24 @@ module TestBench
       end
       attr_writer :buffer
 
+      def sync
+        @sync.nil? ? @sync = true : @sync
+      end
+
       def tty?
         device.tty?
       end
 
       def flush
         buffer.flush(device, alternate_device)
+      end
+
+      def sync=(sync)
+        @sync = sync
+
+        if sync
+          flush
+        end
       end
     end
   end
